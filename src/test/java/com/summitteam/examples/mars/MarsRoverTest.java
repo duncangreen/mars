156f598205259df357a37b96178979df7d6f86dd
Duncan Green <duncan.green@mendeley.com>
2015-07-20 15:27:58 +0100

    makes tests more expressive

--- src/test/java/com/summitteam/examples/mars/MarsRoverTest.java
+++ src/test/java/com/summitteam/examples/mars/MarsRoverTest.java
@@ -2,54 +2,52 @@ package com.summitteam.examples.mars;
 
 import org.junit.Test;
 
+import static com.summitteam.examples.mars.SampleCoordinates.ORIGIN;
 import static org.fest.assertions.Assertions.assertThat;
 
 public class MarsRoverTest {
 
-    public static final PlateauCoordinate POSITION_1_1 = new PlateauCoordinate(1, 1);
-
     @Test
     public void initialPositionAndHeading() {
-        Heading initialHeading = Heading.NORTH;
-        MarsRover rover = new MarsRover(new MarsPlateau(new PlateauCoordinate(5, 5)), POSITION_1_1, initialHeading);
+        MarsRover rover = new MarsRover(new MarsPlateau(new PlateauCoordinate(5, 5)), ORIGIN, Heading.NORTH);
 
-        assertThat(rover.getPosition()).isEqualTo(POSITION_1_1);
-        assertThat(rover.getHeading()).isEqualTo(initialHeading);
+        assertThat(rover.getPosition()).isEqualTo(ORIGIN);
+        assertThat(rover.getHeading()).isEqualTo(Heading.NORTH);
     }
 
     @Test
     public void spinLeft() {
-        MarsRover rover = new MarsRover(new MarsPlateau(new PlateauCoordinate(5, 5)), POSITION_1_1, Heading.NORTH);
+        MarsRover rover = new MarsRover(new MarsPlateau(new PlateauCoordinate(5, 5)), ORIGIN, Heading.NORTH);
 
         rover.spinLeft();
 
-        assertThat(rover.getPosition()).isEqualTo(POSITION_1_1);
+        assertThat(rover.getPosition()).isEqualTo(ORIGIN);
         assertThat(rover.getHeading()).isEqualTo(Heading.WEST);
     }
 
     @Test
     public void spinRight() {
-        MarsRover rover = new MarsRover(new MarsPlateau(new PlateauCoordinate(5, 5)), POSITION_1_1, Heading.NORTH);
+        MarsRover rover = new MarsRover(new MarsPlateau(new PlateauCoordinate(5, 5)), ORIGIN, Heading.NORTH);
 
         rover.spinRight();
 
-        assertThat(rover.getPosition()).isEqualTo(POSITION_1_1);
+        assertThat(rover.getPosition()).isEqualTo(ORIGIN);
         assertThat(rover.getHeading()).isEqualTo(Heading.EAST);
     }
 
     @Test
     public void moveForward() {
-        MarsRover rover = new MarsRover(new MarsPlateau(new PlateauCoordinate(5, 5)), POSITION_1_1, Heading.NORTH);
+        MarsRover rover = new MarsRover(new MarsPlateau(new PlateauCoordinate(5, 5)), ORIGIN, Heading.NORTH);
 
         rover.moveForward();
 
         assertThat(rover.getHeading()).isEqualTo(Heading.NORTH);
-        assertThat(rover.getPosition()).isEqualTo(new PlateauCoordinate(1, 2));
+        assertThat(rover.getPosition()).isEqualTo(ORIGIN.neighbour(Heading.NORTH));
     }
 
     @Test
     public void moveInACircle() {
-        MarsRover rover = new MarsRover(new MarsPlateau(new PlateauCoordinate(5, 5)), POSITION_1_1, Heading.NORTH);
+        MarsRover rover = new MarsRover(new MarsPlateau(new PlateauCoordinate(5, 5)), ORIGIN, Heading.NORTH);
 
         rover.moveForward();
         rover.spinLeft();
@@ -61,6 +59,6 @@ public class MarsRoverTest {
         rover.spinLeft();
 
         assertThat(rover.getHeading()).isEqualTo(Heading.NORTH);
-        assertThat(rover.getPosition()).isEqualTo(POSITION_1_1);
+        assertThat(rover.getPosition()).isEqualTo(ORIGIN);
     }
 }
---
+++ src/test/java/com/summitteam/examples/mars/SampleCoordinates.java
@@ -0,0 +1,5 @@
+package com.summitteam.examples.mars;
+
+public class SampleCoordinates {
+    public static final PlateauCoordinate ORIGIN = new PlateauCoordinate(0, 0);
+}
